eebe50986373dd6b23f08368c825b8ee5baa2e4d
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-04-21 11:24:23 -0500

    update _header.ejs template to display logout when user is signed in and login/register when user is not

--- express_server.js
+++ express_server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser')
+const morgan = require('morgan')
 const { v4: uuidv4 } = require('uuid'); //NPM package that provides a unique identifier
-const e = require("express");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser())
+app.use(morgan('dev'))
 
 
 const PORT = 8080; // setting default port to 8080
@@ -20,22 +21,27 @@ const urlDatabase = {
 };
 
 const users = {
-  'spiderman': {
-    id: uuidv4(),
-    email:"spiderman@gmail.com",
-    password:"spideysense"
+  "clarkKentID": {
+    id: "clarkKentID",
+    email: "superman@example.com",
+    password: "smallville"
   },
-  'superman': {
-    id: uuidv4(),
-    email:"superman@gmail.com",
-    password:"smallville"
-  },
+  "loisLaneID": {
+    id: "loisLaneID",
+    email: "lois@example.com",
+    password: "dailyplanet"
+  }
 };
 
-// ROUTES
+//Function to generate a "unique" 6 character alphanumeric shortURL
+function generateRandomString() {
+  return Math.random().toString(20).substring(2, 8)
+}
+
+//----GET----
+//Routes that render ejs templates
 app.get("/", (req, res) => {
   const templateVars = { urls: urlDatabase, user: users[req.cookies.user_id] };
-  console.log(users[req.cookies.user_id])
   res.render("urls_index", templateVars);
 });
 
@@ -50,7 +56,8 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies.user_id] };
+  const userId = req.cookies['user_id']
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[userId] };
   res.render("urls_show", templateVars);
 });
 
@@ -65,83 +72,94 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
+  const userId = req.cookies['userId'];
   const templateVars = { user:users[req.cookies.user_id] }
   res.render('urls_login', templateVars)
 });
 
-//CREATE
+//----POST----
+// creates new shortURL and stores it in users database.
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
+  console.log('stores:', shortURL)
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
 });
 
-//DELETE
+// deletes existing entire longURL/shortURL entry from users database.
 app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL]
+  const shortURL = req.params.shortURL
+  delete urlDatabase[shortURL]
   res.redirect("/urls")
 });
 
-//UPDATE
+// replaces/updates existing longURL in users database with new longURL from user input. 
 app.post("/urls/:shortURL",(req, res) => {
   const shortURL = req.params.shortURL;
   urlDatabase[shortURL] = req.body.longURL
   res.redirect("/urls");
 });
 
-//USER LOGIN
+// logs in user if an email matches already existing email in users database.
 app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (email === '' || password === '') {
+    return res.status(400).send("Please enter a valid email and password");
+  }
+
+  let foundUser = null;
+  for (const user in users){
+    if (email === users[user].email) {
+      foundUser = user;
+    }
+  } 
+  res.cookie('user_id',foundUser)
   res.redirect("/urls")
 })
 
-//USER REGISTER
+// registers a user if email isnt already in use.
 app.post("/register", (req, res) => {
   const id = uuidv4(); //assign unique identifier with the help of NPM package UUID
   const email = req.body.email;
   const password = req.body.password;
+  
+  // Error condition: if string or password are left empty return error msg.
+  if (email === '' || password === '') {
+    res.status(400).send("Please enter a valid email and password");
+    res.end()
+  }
 
-  // Error condition: if string or password are left empty
-  if (email === '' || password === '') return res.status(400).send("Please enter a valid email and password");
-  
+  // Error condtion: if email address already exists return error msg.
   let foundUser = null;
-  
-  // Error condtion: if email address already exists.
   for (const user in users) {
     if (users[user].email === email) {
-      foundUser = user
+      foundUser = user;
       return res.status(400).send("Email address already exists")
     } 
   }
-
 
   const user = {
     id,
     email,
     password
   };
-
+  
   users[id] = user
-
+  
   res.cookie('user_id',id)
-  res.send("Got your info")
+  res.redirect('/urls')
 });
 
-//USER LOGOUT
+// logs out user by clearing cookie when logout button is clicked.
 app.post("/logout", (req, res) => {
   res.clearCookie('user_id')
-  res.redirect("/urls")
+  res.redirect("/login")
+  res.end()
 })
 
-//SERVER LISTENING ON PORT: 8080
+//----SERVER-----
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-//simulate generating a "unique" shortURL that returns a string of 6 random alphanumeric characters
-function generateRandomString() {
-  return Math.random().toString(20).substring(2, 8)
-}
-
-/* 
-Update all endpoints that currently pass a username value to the templates to pass the entire user object to the template instead:
-*/
